42d66c7a67f9384a25927500641d4a8e88a2838e
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-07-05 11:46:32 -0400

    output path adjustment

--- bin/demo.py
+++ bin/demo.py
@@ -47,11 +47,11 @@ ch = Channel('LA',args.freq)
 lmin = args.lmin
 lmax = args.lmax
 polcomb = args.polcomb
-config = io.config_from_yaml("../input/config.yml")
+config = io.config_from_yaml("input/config.yml")
 
 mask = initialize_mask(nside,smooth_deg)
 solint = SOLensInterface(mask)
-thloc = "../data/" + config['theory_root']
+thloc = "data/" + config['theory_root']
 theory = cosmology.loadTheorySpectraFromCAMB(thloc,get_dimensionless=False)
 
 # norm dict
