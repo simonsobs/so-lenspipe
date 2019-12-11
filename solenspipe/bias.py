da10fe73dc4cf5856ef0a6e4857f03b1703fc494
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-12-11 13:17:53 -0500

    polarization combination split estimator

--- solenspipe/bias.py
+++ solenspipe/bias.py
@@ -125,3 +125,80 @@ def mcmf(icov,alpha,qfunc,get_kmap,comm,nsims):
     totnot = utils.allreduce(ntot,comm) 
     return mftot/totntot
         
+
+def _validate_splits(xyuv,x,y,u,v):
+    xn,yn,un,vn = xyuv
+    nsplits = x.shape[0]
+    assert nsplits>=4
+    for a in [x,y,u,v]:
+        assert a.ndim in [2,4]
+        assert a.shape[0]==nsplits
+    return nsplits
+
+def cross_estimator(xyuv,qe_func,pow_func,xsplits,ysplits,usplits,vsplits,
+                    xcoadd=None,ycoadd=None,ucoadd=None,vcoadd=None):
+    """
+
+    Returns the cross-only estimate of the raw 4-point lensing power spectrum given
+    splits of the data and functions for lensing reconstrucion and power spectrum
+    calculation.
+
+    xyuv: string containing 4-letter lensing power spectrum combination. e.g. TTTT,
+    TTTE, EEEB, etc. These are used in function calls with qe_func.
+
+    qe_func: a function qfunc(xy,x,y) that calculates the un-normalized quadratic estimator
+    between x and y given an estimator name xy. If the input fields are filtered,
+    this function should not apply filtering, and vice versa. The function should
+    not distinguish between whether the input fields are coadds or individual splits,
+    and in particular, any filtering for coadds and splits should be identical.
+    
+    pow_func: A generic function pow_func(x,y) that takes harmonics x and y
+    and converts to a power spectrum. e.g. hp.alm2cl for SHTs and
+    (x*y.conj()).real for 2D FFTs.
+
+    xsplits,ysplits,usplits,vsplits: (nsplits,nalms) or (nsplits,Ny,Nx)
+    arrays containing the splits for each of X,Y,U,V data.
+
+    xcoadd,ycoadd,ucoadd,vcoadd: coadds of above. If not provided, will
+    use np.mean(splits,axis=0).
+
+
+    """
+    X,Y,U,V = xyuv
+    XY = X + Y
+    UV = U + V
+    m = float(_validate_splits(xyuv,xsplits,ysplits,usplits,vsplits))
+    xs = np.asanyarray(xsplits)
+    ys = np.asanyarray(ysplits)
+    us = np.asanyarray(usplits)
+    vs = np.asanyarray(vsplits)
+    cfunc = lambda x: np.mean(x,axis=0)
+    xc = cfunc(xs)
+    yc = cfunc(ys)
+    uc = cfunc(us)
+    vc = cfunc(vs)
+    phi_A_coadd = qe_func(XY,xc,yc)
+    phi_B_coadd = qe_func(UV,uc,vc)
+    sum_phiiiA = 0.
+    sum_phiiiB = 0.
+    sum_CphiixA_phiixB = 0.
+    sum_CphiijA_phiijB = 0.
+    for i in range(int(m)):
+        phiiA = (qe_func(XY,xs[i],yc)+qe_func(XY,xc,ys[i]))/2.
+        phiiB = (qe_func(UV,us[i],vc)+qe_func(UV,uc,vs[i]))/2.
+        phiiiA = qe_func(XY,xs[i],ys[i]) # = (qe_func(XY,xs[i],ys[i])+qe_func(XY,xs[i],ys[i]))/2.
+        phiiiB = qe_func(UV,us[i],vs[i]) # = (qe_func(UV,us[i],vs[i])+qe_func(UV,us[i],vs[i]))/2.
+        sum_phiiiA += phiiiA
+        sum_phiiiB += phiiiB
+        phiixA = phiiA - phiiiA/m
+        phiixB = phiiB - phiiiB/m
+        sum_CphiixA_phiixB += pow_func(phiixA,phiixB)
+        for j in range(i+1,int(m)):
+            phiijA = (qe_func(XY,xs[i],ys[j])+qe_func(XY,xs[j],ys[i]))/2.
+            phiijB = (qe_func(UV,us[i],vs[j])+qe_func(UV,us[j],vs[i]))/2.
+            sum_CphiijA_phiijB += pow_func(phiijA,phiijB)
+    phixA = phi_A_coadd - sum_phiiiA / m**2
+    phixB = phi_B_coadd - sum_phiiiB / m**2
+    C_phixA_phixB = pow_func(phixA,phixB)
+    return ( m**4. * C_phixA_phixB- 4. * m**2. * sum_CphiixA_phiixB + \
+               4. *sum_CphiijA_phiijB ) /m / (m-1.) / (m-2.) / (m-3.)
