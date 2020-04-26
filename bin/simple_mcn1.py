811db655c505d9c5aee41b8b31d75f9324d10fed
ÂFrank Qu <frankqu7@gmail.com>
2020-04-26 11:46:51 -0700

    added mask factors to mcn1 and simple_rdn0

--- bin/simple_mcn1.py
+++ bin/simple_mcn1.py
@@ -10,58 +10,46 @@ from enlib import bench
 import healpy as hp
 from solenspipe import bias
 
-"""
-We will do a simple lensing reconstruction test.
-No mask.
-"""
 
-# Lensing reconstruction ell range
-lmin = 100
-lmax = 3000
+import argparse
+# Parse command line
+parser = argparse.ArgumentParser(description='Do a thing.')
+parser.add_argument("label", type=str,help='Label.')
+parser.add_argument("polcomb", type=str,help='polcomb.')
+parser.add_argument("-N", "--nsims",     type=int,  default=10,help="Number of sims.")
+parser.add_argument("--sindex",     type=int,  default=0,help="Declination band.")
+parser.add_argument("--lmin",     type=int,  default=100,help="Minimum multipole.")
+parser.add_argument("--lmax",     type=int,  default=3000,help="Minimum multipole.")
+parser.add_argument("--isotropic", action='store_true',help='Isotropic sims.')
+parser.add_argument("--no-atmosphere", action='store_true',help='Disable atmospheric noise.')
+parser.add_argument("--use-cached-norm", action='store_false',help='Use  cached norm.')
+parser.add_argument("--wnoise",     type=float,  default=None,help="Override white noise.")
+parser.add_argument("--beam",     type=float,  default=None,help="Override beam.")
+parser.add_argument("--disable-noise", action='store_true',help='Disable noise.')
+parser.add_argument("--healpix", action='store_true',help='Use healpix.')
+parser.add_argument("--no-mask", action='store_true',help='No mask. Use with the isotropic flag.')
+parser.add_argument("--debug", action='store_true',help='Debug plots.')
+parser.add_argument("--flat-sky-norm", action='store_true',help='Use flat-sky norm.')
+args = parser.parse_args()
 
-polcomb = 'EE'
-
-# Number of sims
-nsims = 100
-
-# CAR resolution is decided based on lmax
-res = np.deg2rad(2.0 *(3000/lmax) /60.)
-
-# Make the full sky geometry
-shape,wcs = enmap.fullsky_geometry(res=res)
-
-# We use a mask of ones for this test
-mask = enmap.ones(shape,wcs)
-
-# Initialize the lens simulation interface
-solint = solenspipe.SOLensInterface(mask=mask,data_mode=None,scanning_strategy="isotropic",fsky=0.4)
+solint,ils,Als,Nl,comm,rank,my_tasks,sindex,debug_cmb,lmin,lmax,polcomb,nsims,channel,isostr = solenspipe.initialize_args(args)
       
 
-# Choose the frequency channel
-channel = mapsims.SOChannel("LA", 145)
-
-# norm dict
-Als = {}
-ls,Als['TT'],Als['EE'],Als['EB'],Als['TE'],Als['TB'],al_mv_pol,al_mv,Al_te_hdv = solenspipe.initialize_norm(solint,channel,lmin,lmax)
-Als['mv'] = al_mv
-Als['mvpol'] = al_mv_pol
-al_mv = Als[polcomb]
-
-comm,rank,my_tasks = mpi.distribute(nsims)
-
+w4 = solint.wfactor(4)
 get_kmap = lambda seed: solint.get_kmap(channel,seed,lmin,lmax,filtered=True)
 power = lambda x,y: hp.alm2cl(x,y)
 qfunc = solint.qfunc
 
-assert ls[0]==0
-assert len(ls) == Als[polcomb].size
-nmax = len(ls)
+nmax = len(ils)
 
 mcn1 = bias.mcn1(0,polcomb,polcomb,qfunc,get_kmap,comm,power,nsims,verbose=True)
+mcn1[:nmax] = mcn1[:nmax] * Als[polcomb]**2. 
+if not(args.no_mask):
+    mcn1[:nmax] = mcn1[:nmax]/w4
+    
 
-mcn1[:nmax] = mcn1[:nmax] * Als[polcomb]**2.  #why not multiply by 0.25
 mcn1[nmax:] = 0
-io.save_cols(f'{solenspipe.opath}/n1_ee.txt',(ls,mcn1[:nmax]))
+io.save_cols(f'{solenspipe.opath}/n1_{polcomb}.txt',(ls,mcn1[:nmax]))
 
 
 if rank==0:
--- bin/simple_rdn0.py
+++ bin/simple_rdn0.py
@@ -16,13 +16,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Do a thing.')
 parser.add_argument("label", type=str,help='Label.')
 parser.add_argument("polcomb", type=str,help='polcomb.')
-parser.add_argument("-N", "--nsims",     type=int,  default=1,help="Number of sims.")
+parser.add_argument("-N", "--nsims",     type=int,  default=10,help="Number of sims.")
 parser.add_argument("--sindex",     type=int,  default=0,help="Declination band.")
 parser.add_argument("--lmin",     type=int,  default=100,help="Minimum multipole.")
 parser.add_argument("--lmax",     type=int,  default=3000,help="Minimum multipole.")
 parser.add_argument("--isotropic", action='store_true',help='Isotropic sims.')
 parser.add_argument("--no-atmosphere", action='store_true',help='Disable atmospheric noise.')
-parser.add_argument("--use-cached-norm", action='store_true',help='Use  cached norm.')
+parser.add_argument("--use-cached-norm", action='store_false',help='Use  cached norm.')
 parser.add_argument("--wnoise",     type=float,  default=None,help="Override white noise.")
 parser.add_argument("--beam",     type=float,  default=None,help="Override beam.")
 parser.add_argument("--disable-noise", action='store_true',help='Disable noise.')
@@ -35,7 +35,7 @@ args = parser.parse_args()
 solint,ils,Als,Nl,comm,rank,my_tasks,sindex,debug_cmb,lmin,lmax,polcomb,nsims,channel,isostr = solenspipe.initialize_args(args)
       
 
-
+w4 = solint.wfactor(4)
 get_kmap = lambda seed: solint.get_kmap(channel,seed,lmin,lmax,filtered=True)
 power = lambda x,y: hp.alm2cl(x,y)
 qfunc = solint.qfunc
@@ -46,8 +46,10 @@ rdn0 = bias.rdn0(icov=0,alpha=polcomb,beta=polcomb,qfunc=qfunc,get_kmap=get_kmap
 #rdn0 = bias.mcn1(0,'TT','TT',qfunc,get_kmap,comm,power,nsims,verbose=True)
 
 rdn0[:nmax] = rdn0[:nmax] * Als[polcomb]**2.
+if not(args.no_mask):
+    rdn0[:nmax]=rdn0[:nmax]/w4
 rdn0[nmax:] = 0
-io.save_cols(f'{solenspipe.opath}/rdn0_tt.txt',(ils,rdn0[:nmax]))
+io.save_cols(f'{solenspipe.opath}/rdn0_{polcomb}.txt',(ils,rdn0[:nmax]))
 
 
 if rank==0:
