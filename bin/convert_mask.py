extend rdn0 to include realization dependent mean-field subtraction; extend quicklens norm interface to include flat-sky calculation ; fix bug in CAR mask initialization

bin/convert_mask.py
@@ -5,17 +5,8 @@ import numpy as np
 import os,sys
 import healpy as hp
 
-def galactic_mask(shape,wcs,nside,theta1,theta2):
-    npix = hp.nside2npix(nside)
-    orig = np.ones(npix)
-    orig[hp.query_strip(nside,theta1,theta2)] = 0
-    return reproject.ivar_hp_to_cyl(orig, shape, wcs, rot=True,do_mask=False,extensive=False)
+from orphics.maps import galactic_mask, north_galactic_mask, south_galactic_mask
 
-def north_galactic_mask(shape,wcs,nside):
-    return galactic_mask(shape,wcs,nside,0,np.deg2rad(90))
-
-def south_galactic_mask(shape,wcs,nside):
-    return galactic_mask(shape,wcs,nside,np.deg2rad(90),np.deg2rad(180))
 
 
 config = io.config_from_yaml(os.path.dirname(os.path.abspath(__file__)) + "/../input/config.yml")

solenspipe/bias.py
@@ -41,13 +41,22 @@ elif set==2 or set==3:
 =================
 """
     
-def rdn0(icov,alpha,beta,qfunc,get_kmap,comm,power,nsims):
+def rdn0(icov,alpha,beta,qfunc,get_kmap,comm,power,nsims,include_meanfield=True,include_gauss=True,multisim=False):
     """
-    RDN0 for alpha=XY cross beta=AB
-    qfunc(XY,x,y) returns QE XY reconstruction minus mean-field in fourier space
+    Anisotropic MC-RDN0 for alpha=XY cross beta=AB
+    qfunc(XY,x,y) returns QE XY reconstruction 
     get_kmap("T",(0,0,1)
 
     e.g. rdn0(0,"TT","TE",qest.get_kappa,get_kmap,comm,power)
+
+    By default, include_meanfield=True, this corrects for both the data and sim mean-field, so no map-level
+    subtraction of mean-field is required. However, you typically want
+    the mean-field to be more converged, so you could run it separately with higher nsims
+    (setting include_gauss=False), and then do a different run with include_meanfield=False, include_gauss=True
+    with lower number of nsims.
+
+    multisim=True can be used with include_meanfield=False to obtain the Sherwin et. al. 1611.09753
+    Eq 7 calculation (which does not correct for the data mean-field).
     """
     eX,eY = alpha
     eA,eB = beta
@@ -65,12 +74,18 @@ def rdn0(icov,alpha,beta,qfunc,get_kmap,comm,power,nsims):
         Ys  = get_kmap((icov,0,i))
         As  = get_kmap((icov,0,i))
         Bs  = get_kmap((icov,0,i))
-        Ysp = get_kmap((icov,1,i))
-        Asp = get_kmap((icov,1,i))
-        Bsp = get_kmap((icov,1,i))
-        rdn0 += power(qa(X,Ys),qb(A,Bs)) + power(qa(Xs,Y),qb(A,Bs)) \
-            + power(qa(Xs,Y),qb(As,B)) + power(qa(X,Ys),qb(As,B)) \
-            - power(qa(Xs,Ysp),qb(As,Bsp)) - power(qa(Xs,Ysp),qb(Asp,Bs))
+        if include_gauss:
+            rdn0 += power(qa(X,Ys),qb(A,Bs)) + power(qa(Xs,Y),qb(A,Bs)) \
+                   + power(qa(Xs,Y),qb(As,B)) + power(qa(X,Ys),qb(As,B))
+        if include_meanfield:
+            rdn0 += power(qa(Xs,Ys),qb(A,B)) + power(qa(X,Y),qb(As,Bs))
+        if multisim:
+            Ysp = get_kmap((icov,1,i))
+            Asp = get_kmap((icov,1,i))
+            Bsp = get_kmap((icov,1,i))
+            rdn0 += (- power(qa(Xs,Ysp),qb(As,Bsp)) - power(qa(Xs,Ysp),qb(Asp,Bs)))
+        else:
+            rdn0 +=  (-power(qa(Xs,Ys),qb(As,Bs)))
     totrdn0 = utils.allreduce(rdn0,comm) 
     return totrdn0/nsims
 
@@ -254,10 +269,12 @@ def cross_estimator_symbolic(xyuv,nsplits,qe_func,pow_func,
                4. *sum_CphiijA_phiijB ) /m / (m-1.) / (m-2.) / (m-3.)
 
 
-def get_feed_dict(shape,wcs,theory,noise_t,noise_p,fwhm):
+def get_feed_dict(shape,wcs,theory,noise_t,noise_p,fwhm,gtfunc=None):
+    from pixell import enmap
+    import symlens
     modlmap = enmap.modlmap(shape,wcs)
     feed_dict = {}
-    feed_dict['uC_T_T'] = theory.lCl('TT',modlmap)
+    feed_dict['uC_T_T'] = theory.lCl('TT',modlmap) if (gtfunc is None) else gtfunc(modlmap)
     feed_dict['uC_T_E'] = theory.lCl('TE',modlmap)
     feed_dict['uC_E_E'] = theory.lCl('EE',modlmap)
     feed_dict['tC_T_T'] = theory.lCl('TT',modlmap) + (noise_t * np.pi/180./60.)**2. / symlens.gauss_beam(modlmap,fwhm)**2.
@@ -271,8 +288,10 @@ def get_feed_dict(shape,wcs,theory,noise_t,noise_p,fwhm):
     return feed_dict
 
 
-def RDN0_analytic(shape,wcs,theory,fwhm,noise_t,noise_p,powdict,estimator,XY,UV,xmask,ymask,kmask,AXY,AUV,split_estimator=False):
-    feed_dict = get_feed_dict(shape,wcs,theory,noise_t,noise_p,fwhm)
+def RDN0_analytic(shape,wcs,theory,fwhm,noise_t,noise_p,powdict,estimator,XY,UV,
+                  xmask,ymask,kmask,AXY,AUV,split_estimator=False,gtfunc=None):
+    import symlens
+    feed_dict = get_feed_dict(shape,wcs,theory,noise_t,noise_p,fwhm,gtfunc=gtfunc)
     feed_dict['dC_T_T'] = powdict['TT']
     feed_dict['dC_T_E'] = powdict['TE']
     feed_dict['dC_E_E'] = powdict['EE']

solenspipe/solenspipe.py
@@ -55,7 +55,7 @@ def initialize_args(args):
         else:
             deg = 2
             afname = f'{opath}/car_mask_lmax_{lmax}_apodized_{deg:.1f}_deg.fits'
-            mask = enmap.read_map(afname) 
+            mask = enmap.read_map(afname)[0]
 
 
     # Initialize the lens simulation interface
@@ -396,7 +396,8 @@ def initialize_mask(nside,smooth_deg):
 
         
 
-def quicklens_norm(polcomb,nltt,nlee,nlbb,theory,tellmin,tellmax,pellmin,pellmax,Lmax=None):
+def quicklens_norm(polcomb,nltt,nlee,nlbb,theory,tellmin,tellmax,pellmin,pellmax,Lmax=None,
+                   flatsky=False,flatsky_nx=2048,flatsky_dx_arcmin=2.0,flatsky_bin_edges=None):
     import quicklens as ql
     fmap = {'TT':ql.qest.lens.phi_TT,
             'TE':ql.qest.lens.phi_TE,
@@ -439,11 +440,27 @@ def quicklens_norm(polcomb,nltt,nlee,nlbb,theory,tellmin,tellmax,pellmin,pellmax
         fly[ls<pellmin] = 0
         fly[ls>pellmax] = 0
 
-    resp_fullsky = qest.fill_resp(qest, np.zeros(Lmax+1, dtype=np.complex), flx, fly)
-    nlqq_fullsky = 1 / resp_fullsky
-    if X!=Y: nlqq_fullsky = nlqq_fullsky / 2.
 
-    return ls,(ls*(ls+1.)) * nlqq_fullsky.real
+    if flatsky:
+        nx         = flatsky_nx  # number of pixels for flat-sky calc.
+        dx         = flatsky_dx_arcmin/60./180.*np.pi # pixel width in radians.
+        pix        = ql.maps.pix(nx,dx)
+        clxy = theory.lCl(X+Y,ls)
+        resp = qest.fill_resp(qest, ql.maps.cfft(nx, dx), flx, fly)
+    else:
+        resp = qest.fill_resp(qest, np.zeros(Lmax+1, dtype=np.complex), flx, fly)
+    nlqq = 1 / resp
+
+    if X!=Y: nlqq = nlqq / 2.
+
+    if flatsky:
+        t          = lambda l: (l*(l+1.))
+        bcl = nlqq.get_ml(flatsky_bin_edges, t=t)
+        ls = bcl.ls
+        return ls,bcl.specs['cl']
+    else:
+        ret = nlqq.real
+        return ls,(ls*(ls+1.)) * ret
 
 	
 def checkproc_py():