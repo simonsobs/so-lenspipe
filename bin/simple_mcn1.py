be1f43636a7fea9ebfd64017150e25e17f46d98b
ÂFrank Qu <frankqu7@gmail.com>
2020-09-26 14:13:59 -0700

    curl option for mcn1

--- bin/simple_mcn1.py
+++ bin/simple_mcn1.py
@@ -14,10 +14,10 @@ from solenspipe import bias
 import argparse
 # Parse command line
 parser = argparse.ArgumentParser(description='Do a thing.')
-parser.add_argument("label", type=str,help='Label.')
-parser.add_argument("polcomb", type=str,help='polcomb.')
-parser.add_argument("-N", "--nsims",     type=int,  default=500,help="Number of sims.")
-parser.add_argument("--sindex",     type=int,  default=0,help="Declination band.")
+parser.add_argument("label", type=str,help='Version label.')
+parser.add_argument("polcomb", type=str,help='Polarizaiton combination: one of mv,TT,TE,EB,TB,EE.')
+parser.add_argument("-N", "--nsims",     type=int,  default=100,help="Number of sims.")
+parser.add_argument("--sindex",     type=int,  default=0,help="Start index for sims.")
 parser.add_argument("--lmin",     type=int,  default=100,help="Minimum multipole.")
 parser.add_argument("--lmax",     type=int,  default=3000,help="Minimum multipole.")
 parser.add_argument("--isotropic", action='store_true',help='Isotropic sims.')
@@ -27,11 +27,17 @@ parser.add_argument("--wnoise",     type=float,  default=None,help="Override whi
 parser.add_argument("--beam",     type=float,  default=None,help="Override beam.")
 parser.add_argument("--disable-noise", action='store_true',help='Disable noise.')
 parser.add_argument("--zero-sim", action='store_true',help='Just make a sim of zeros. Useful for benchmarking.')
-parser.add_argument("--healpix", action='store_true',help='Use healpix.')
+parser.add_argument("--write-meanfield", action='store_true',help='Calculate and save mean-field map.')
+parser.add_argument("--read-meanfield", action='store_true',help='Read and subtract mean-field map.')
+parser.add_argument("--healpix", action='store_true',help='Use healpix instead of CAR.')
 parser.add_argument("--no-mask", action='store_true',help='No mask. Use with the isotropic flag.')
 parser.add_argument("--debug", action='store_true',help='Debug plots.')
 parser.add_argument("--flat-sky-norm", action='store_true',help='Use flat-sky norm.')
+parser.add_argument("--ps_bias_hardening", action='store_true',help='TT point source hardening estimator.')
+parser.add_argument("--mask_bias_hardening", action='store_true',help='TT mask hardening estimator.')
+parser.add_argument("--curl", action='store_true',help='curl reconstruction')
 args = parser.parse_args()
+
 
 solint,ils,Als,Nl,comm,rank,my_tasks,sindex,debug_cmb,lmin,lmax,polcomb,nsims,channel,isostr = solenspipe.initialize_args(args)
 car = "healpix_" if args.healpix else "car_"
@@ -40,8 +46,10 @@ car = "healpix_" if args.healpix else "car_"
 w4 = solint.wfactor(4)
 get_kmap = lambda seed: solint.get_kmap(channel,seed,lmin,lmax,filtered=True)
 power = lambda x,y: hp.alm2cl(x,y)
-qfunc = solint.qfunc
-
+if args.curl:
+    qfunc = solint.qfunc_curl
+else:
+    qfunc = solint.qfunc
 nmax = len(ils)
 
 mcn1 = bias.mcn1(0,polcomb,polcomb,qfunc,get_kmap,comm,power,nsims,verbose=True)
@@ -50,8 +58,11 @@ if not(args.no_mask):
     mcn1[:nmax] = mcn1[:nmax]/w4
     
 mcn1[nmax:] = 0
-io.save_cols(f'{solenspipe.opath}/n1mc_{args.polcomb}_{isostr}_{car}_{nsims}.txt',(ils,mcn1[:nmax]))
+if args.curl:
+	io.save_cols(f'{solenspipe.opath}/n1mc_curl_{args.polcomb}_{isostr}_{nsims}_{args.label}.txt',(ils,mcn1[:nmax]))
 
+else:
+	io.save_cols(f'{solenspipe.opath}/n1mc_{args.polcomb}_{isostr}_{nsims}_{args.label}.txt',(ils,mcn1[:nmax]))
 
 if rank==0:
     theory = cosmology.default_theory()
