e2c331a043f878d4c6a51853df6c52613f74150a
NiallMac <niall.maccrann@gmail.com>
2021-03-26 09:02:39 -0700

    First demo script for ksz stuff

---
+++ examples/verify_ksz.py
@@ -0,0 +1,20 @@
+#Run ksz estimator on lensing sims
+
+#Import stuff
+
+#Read in sim alms
+
+#Filter with with W_S,i from eqn. 1 of 1803.07036
+
+#Get the "qfuncs" - falafel point-source estimators
+#run on the maps
+get_qfunc():
+    return qe.point_sources
+
+#Get the point-source normalization from pytempura,
+#using filter from above
+
+
+#normalize the etimates
+
+#run rdn0 using the qfuncs above
